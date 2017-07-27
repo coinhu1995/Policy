b5035566d2ec7b94552b08b0045472f09ce6b5a9
uhn <coinhu1995@gmail.com>
2017-07-27 22:26:01 +0700

    79%

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -14,8 +14,9 @@ public class test {
 		SQLDAO cmtDao = SQLDAOFactory.getDAO(SQLDAOFactory.COMMENT);
 		SQLDAO cmtTestDao = SQLDAOFactory.getDAO(SQLDAOFactory.COMMENTTEST);
 		Comment c = new Comment();
-		c.setContent("chính sách hay tôi tán thành");
+		c.setContent("thật là nực cười quá");
 		c.setCmt_segment(ss.sentence2SegmentSentence(c.getContent(), SentenceSegment.VNTOKENIZER));
+		c.setLabel(2);
 		cmtDao.insert(c);
 		System.out.println("done");
 	}
